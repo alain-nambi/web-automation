56a1adfe072dd1ca0172a8908cba52c504debfd3
alain-nambi <alainnambi@gmaiL.com>
2023-12-28 16:11:22 +0300

    feat: update server.js * Add environment type * Handling errors by adding specific error type * Remove console.log lines

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const port = process.env.PORT || 3002;
 
 // Constantes pour les URL et les informations d'identification
+const ENVIRONMENT_TYPE = process.env.ENVIRONMENT_TYPE
 const LOGIN_URL = process.env.LOGIN_URL;
 const USERNAME_EWA = process.env.USERNAME_EWA;
 const PASSWORD_EWA = process.env.PASSWORD_EWA;
@@ -38,55 +39,73 @@ const SELECTORS = {
 
 const PNR_NOT_AVAILABLE = ['Annulé', 'Remboursé']
 
+async function launchBrowser() {
+    ENVIRONMENT_TYPE === 'test'
+        ? await chromium.launch({ headless: false })
+        : await chromium.launch({ headless: true })
+}
+
 app.post('/send', async (req, res) => {
     const { pnrNumber, username, password, companyCode } = req.body;
 
-    async function launchBrowser() {
-        return await chromium.launch({ headless: false });
+    if (!username || !password) {
+        return res.status(401).json({
+            message: 'Please enter a username and password'
+        })
+    }
+
+    if (!companyCode) {
+        return res.status(401).json({
+            message: 'Please enter a company code'
+        })
+    }
+
+    if (!pnrNumber) {
+        return res.status(400).json({
+            message: 'Please enter a PNR number'
+        })
     }
 
     async function login(page) {
-        console.info('|> Login ongoing...');
         await page.goto(LOGIN_URL);
-        await page.type('#login', username || USERNAME_EWA);
-        await page.type('#pwd', password || PASSWORD_EWA);
-        await page.type('#LoginCompanyIdentificationCode', companyCode || COMPANY_CODE);
+        await page.type('#login', username);
+        await page.type('#pwd', password);
+        await page.type('#LoginCompanyIdentificationCode', companyCode);
         await page.click('#signInButton');
     }
 
     async function sendPnrPDFMail(page) {
-        console.info('|> Searching PNR...');
-        const searchInput = await page.waitForSelector(SELECTORS.searchInput);
-        await searchInput.type(pnrNumber);
-        await searchInput.press('Enter');
+        try {
+            const searchInput = await page.waitForSelector(SELECTORS.searchInput);
+            await searchInput.type(pnrNumber);
+            await searchInput.press('Enter');
 
-        console.info('|> Find mainframe...');
-        const mainFrame = await page.frameLocator(SELECTORS.mainFrame);
-        const pnrStatusElement = await mainFrame.locator(SELECTORS.pnrStatusElement);
-        const pnrStatusText = await pnrStatusElement.innerText();
+            const mainFrame = await page.frameLocator(SELECTORS.mainFrame);
+            const pnrStatusElement = await mainFrame.locator(SELECTORS.pnrStatusElement);
+            const pnrStatusText = await pnrStatusElement.innerText();
 
-        if (PNR_NOT_AVAILABLE.includes(String(pnrStatusText))) {
-            console.info("|> PNR has been cancelled [x]");
-            return 'cancelled';
-        } else {
-            console.info("PNR found...");
-            console.log('|> PNR non annulé...[o]');
-            await mainFrame.locator(SELECTORS.confirmationEmailButton).click();
+            if (PNR_NOT_AVAILABLE.includes(String(pnrStatusText))) {
+                return 'cancelled';
+            } else {
+                await mainFrame.locator(SELECTORS.confirmationEmailButton).click();
 
-            const textAreaSendConfirmationEmissions = await mainFrame.locator(SELECTORS.textAreaSendConfirmationEmissions);
-            await textAreaSendConfirmationEmissions.clear()
-            await textAreaSendConfirmationEmissions.fill(EMAIL_RECIPIENT);
+                const textAreaSendConfirmationEmissions = await mainFrame.locator(SELECTORS.textAreaSendConfirmationEmissions);
+                await textAreaSendConfirmationEmissions.clear()
+                await textAreaSendConfirmationEmissions.fill(EMAIL_RECIPIENT);
 
-            await mainFrame.locator(SELECTORS.sendEmailButton).click();
+                await mainFrame.locator(SELECTORS.sendEmailButton).click();
 
-            return 'available';
+                return 'available';
+            }
+        } catch (error) {
+            return res.status(500).json({
+                message: "Error while sending PNR mail",
+                error: error.message
+            })
         }
     }
 
     async function sendReceiptPnrMail(page) {
-        console.info('|> Searching for PNR Receipt...')
-
-        console.info('|> Find mainframe...');
         const mainFrame = await page.frameLocator(SELECTORS.mainFrame);
         await mainFrame.locator(SELECTORS.printPNRReceiptButton).click();
 
@@ -99,10 +118,6 @@ app.post('/send', async (req, res) => {
 
         await mainFrame.locator("#instanceCtrlContent_ctrlEnvoiEmail_btEnvoyerEmail").click()
 
-        // const textAreaSendPNRReceipt = await locator(SELECTORS.textAreaSendPNRReceipt)
-        // await textAreaSendPNRReceipt.clear()
-        // await textAreaSendPNRReceipt.fill(EMAIL_RECIPIENT);
-
         return 'available';
     }
 
@@ -114,7 +129,7 @@ app.post('/send', async (req, res) => {
         try {
             await login(page);
             const PNRHasBeen = await sendPnrPDFMail(page);
-            
+
             if (PNRHasBeen === 'cancelled') {
                 return res.status(404).json({ status: 'cancelled', pnrNumber });
             } else {
@@ -135,7 +150,6 @@ app.post('/send', async (req, res) => {
                 }
             }
         } catch (error) {
-            console.error(`Failed to send email: ${error.message}`);
             return res.status(500).json({ error: `Failed to send email: ${error.message}` });
         } finally {
             await browser.close();
